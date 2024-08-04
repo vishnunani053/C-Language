c6bee18a8f31a5e832bb99751542672c39e9d358
vishnunani053 <vishnulucky053@gmail.com>
2024-08-04 12:14:09 +0530

    first commit

---
+++ operators/ascii.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    char letter;
+    printf("Enter a character value:");
+    scanf("%c",&letter);
+    printf("The ASCII value of char is %d",letter);
+    return 0;
+}
